b29acf3c3ac96e94d2dd25b4b2b5294bd76eeff4
isaacscardoso <isaacscardosoblues@outlook.com>
2023-01-21 16:06:42 -0300

    Project created.

---
+++ expenses_diary_app/android/app/src/main/kotlin/blueshrine/com/br/expenses_diary_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package blueshrine.com.br.expenses_diary_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ expenses_diary_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expenses Diary'),
+      ),
+      body: const Center(
+        child: Text('Under development...'),
+      ),
+    );
+  }
+}
